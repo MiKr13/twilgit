3446d15b0cfa84cb7a1ebde7e430a37f071c681d
Mihir Kumar <mihirpandey.13@gmail.com>
2021-08-23 10:52:25 +0000

    style(twilgit-remote): :lipstick: Semi colon standard implemented

--- src/plugins/sensible.js
+++ src/plugins/sensible.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
-"use strict"
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
 
 /**
  * This plugins adds some utilities to handle http errors
@@ -9,7 +9,7 @@ const fp = require('fastify-plugin')
  * @see https://github.com/fastify/fastify-sensible
  */
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('fastify-sensible'), {
-    errorHandler: false
-  })
-})
+	fastify.register(require("fastify-sensible"), {
+		errorHandler: false,
+	});
+});
--- src/services/github.js
+++ src/services/github.js
@@ -1,22 +1,20 @@
-const { Octokit } = require("@octokit/core")
+const { Octokit } = require("@octokit/core");
 
 export default class GithubService {
+	#githubPersonalToken = process.env.APP_GITHUB_PERSONAL_TOKEN;
 
-    #githubPersonalToken = process.env.APP_GITHUB_PERSONAL_TOKEN;
+	constructor() {
+		this.oktokit = new Octokit({
+			auth: this.#githubPersonalToken,
+		});
+	}
 
-    constructor() {
-        this.oktokit = new Octokit({
-            auth: this.#githubPersonalToken,
-        })
-    }
-
-    async getRepoList() {
-        try {
-            const { data } = await this.oktokit.request('/user/repos')
-            return data
-        } catch (error) {
-            throw new Error(error.toString())
-        }
-    }
-
+	async getRepoList() {
+		try {
+			const { data } = await this.oktokit.request("/user/repos");
+			return data;
+		} catch (error) {
+			throw new Error(error.toString());
+		}
+	}
 }
